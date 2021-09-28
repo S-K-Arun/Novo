10e4d1b944bc8948989728df66dc4d1f3d6dcfc6
S-K-Arun <skarunpkt@gmail.com>
2021-09-28 19:51:59 +0530

    initial commit

--- viewer/settings.py
+++ viewer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h98fjg+o&bz)emqfv^$nc%4-d7h)vw)qwt+v#=@tj!81#j$bqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','novolytics.herokuapp.com']
 
 
 # Application definition
