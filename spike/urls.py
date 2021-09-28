838814e807a2da5de2372da37a7f64ae83514447
S-K-Arun <skarunpkt@gmail.com>
2021-09-28 19:27:36 +0530

    second commit

--- spike/urls.py
+++ spike/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 from spike.views import Mainview
 from spike import views
 
@@ -10,5 +13,7 @@ urlpatterns = [
     path('Art',views.art,name='Art'),
     path('Buildings',views.buildings,name='Buildings'),
     path('Cars',views.cars,name='Cars'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]
--- viewer/settings.py
+++ viewer/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h98fjg+o&bz)emqfv^$nc%4-d7h)vw)qwt+v#=@tj!81#j$bqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
